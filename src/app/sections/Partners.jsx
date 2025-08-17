d8e5f034b938d2484aeb3882961f26057ae69172
sehamanter1 <sehamanter2@gmail.com>
2025-08-17 15:05:52 +0300

    partners responsive

--- src/app/sections/Partners.jsx
+++ src/app/sections/Partners.jsx
@@ -4,14 +4,14 @@ import { logo_0, logo_1, logo_2, logo_3, logo_4, logo_5 } from '../../assets/ima
 export default function Partners() {
     const logos = [logo_0,logo_1,logo_2,logo_3,logo_4,logo_5]
 return (
-  <section className="container">
-      <div className="carousel-wrapper">
-        <div className="carousel-track gap-[90px]">
+  <section className="container overflow-x-hidden">
+      <div className="carousel-wrapper ">
+        <div className="carousel-track gap-[20px] sm:gap-[40px] md:gap-[90px]">
           {[...logos, ...logos].map((logo, index) => (
             <div className="card" key={index}>
               <img
                 src={logo}
-                className="w-[100px] h-auto object-contain"
+                className="w-[60px] md:w-[80px] lg:w-[100px] h-auto object-contain"
                 alt={`partner-${index}`}
               />
             </div>
