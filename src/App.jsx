8f66b9512ef5f8b7781c573c3c3e2fc168b35125
sehamanter1 <sehamanter2@gmail.com>
2025-08-21 17:48:57 +0300

    remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, useContext, useEffect } from "react";
-
-const Layout = lazy(() => import("./Layout.jsx"));
-// pages
-
-// Not_Found
-const Not_Found = lazy(() => import("./app/pages/Not_Found"));
 const Hero = lazy(() => import("./app/sections/Hero.jsx"));
-
-
-import { bg_icon } from "./assets/Icons.jsx";
-import Main_Header from "./app/root/Main_Header.jsx";
+import Navbar from "./app/root/Navbar.jsx";
 import Values from "./app/sections/Values.jsx";
 import Partners from "./app/sections/Partners.jsx";
 import Services from "./app/sections/Services.jsx";
-import Offers_One from "./app/sections/Offers_One.jsx";
+import Offers_One from "./app/sections/Offers_one.jsx";
 import Offers_Two from "./app/sections/Offers_Two.jsx";
-
 function App() {
     return (
         <BrowserRouter>
             <div className="layout grid gap-[40px] sm:gap-[60px] lg:gap-[80px]">
-                <Main_Header />
+                <Navbar />
                 <Hero />
                 <Values />
                 <Partners />
--- src/app/root/Navbar.jsx
+++ src/app/root/Navbar.jsx
@@ -1,41 +1,51 @@
-import {useTranslation} from "react-i18next";
-import {NavLink} from "react-router-dom";
-import Icon_Chip from "../components/chips/Icon_Chip";
+import { useEffect, useState } from "react";
+import { full_logo } from "../../assets/images/Images";
+import { useTranslation } from "react-i18next";
+import Button from "../components/Button";
+import { NavLink } from "react-router-dom";
+import { Close_menu_icon, Menu_icon } from "../../assets/Icons";
 
 export default function Navbar() {
+    const { t } = useTranslation();
     const navItems = [
-        {path: "/Why Us", label: "home"},
-        {path: "/Services", label: "tasks"},
-        {path: "/Our Process",  label: "tickets"},
-        {path: "/Payments",  label: "reports"},
-        {path: "/FAQs", label: "portfolio"},
+        { label: "Why Us", path: "#why-us" },
+        { label: "Services", path: "#services" },
+        { label: "Our Process", path: "#our-services" },
+        { label: "Payments", path: "#payments" },
+        { label: "FAQs", path: "#faqs" },
     ];
-    const {t} = useTranslation();
+    const [collapse, setCollapse] = useState(false);
+
     return (
-        <div>
-            <ul className="nav_items w-fit  px-[32px] grid items-center gap-[8px] ">
-                {navItems.map((item) => (
-                    <NavLink to={item.path} key={item.path} className={"group"}>
-                        {({isActive}) => (
-                            <div className={`relative flex`}>
-                                <Icon_Chip
-                                    background_color={`${
-                                        isActive ? "bg-background" : "bg-foreground -secondary-variant"
-                                    }`}
-                                    icon={item.icon(
-                                        isActive ? "var(--foreground)" : "var(--background)"
-                                    )}
-                                />
+        <div className="container  mt-[32px]">
+            <div className="navbar h-[48px] mx-auto w-full flex items-center justify-between">
+                <img src={full_logo} alt="logo" className="w-[140px]" />
+
+                <ul className={`${collapse ? "max-md:grid max-md:px-6 max-md:absolute max-md:bg-foreground max-md:h-[100vh] max-md:top-0 content-start max-md:gap-14 max-md:z-[9999] max-md:pt-[100px] !w-[280px] end-0" : "max-md:hidden"} flex w-fit flex mx-auto items-center gap-[16px] xl:gap-[32px]`}>
+                    {navItems.map((item) => (
+                        <NavLink to={item.path} key={item.path} className={"group"}>
+                            {({ isActive }) => (
                                 <li
-                                    className={` transition-all duration-300 absolute start-14 top-3 opacity-0 group-hover:opacity-100 `}
+                                    className={` transition-all font-medium text-background duration-300 group-hover:opacity-100 `}
                                 >
                                     {t(item.label)}
                                 </li>
-                            </div>
-                        )}
-                    </NavLink>
-                ))}
-            </ul>
+                            )}
+                        </NavLink>
+                    ))}
+                    <Button variant="secondary" className={"md:hidden"} text={"Contact us"} />
+                </ul>
+                <span className="md:hidden w-4 ms-0 relative z-[99999]"
+                    onClick={() => setCollapse(!collapse)}
+                    role="button">
+                    {!collapse ?
+                        <Menu_icon />
+                        :
+                        <Close_menu_icon />
+                    }
+                </span>
+                <Button variant="outline" className={"max-md:hidden"} text={"Contact us"} />
+            </div>
         </div>
     );
 }
