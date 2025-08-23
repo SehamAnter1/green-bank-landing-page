7e3c3cae5a422b17e4564a148c8d940f3871fcee
sehamanter1 <sehamanter2@gmail.com>
2025-08-23 08:43:38 +0300

    chore : adjust eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,37 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import', 'unused-imports'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+
+    // catch unused imports & vars
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
+
+    // catch unresolved imports
+    'import/no-unresolved': 'error',
+    'import/no-extraneous-dependencies': 'error',
+
+    // catch undefined variables
+    'no-undef': 'error',
   },
-}
+};
